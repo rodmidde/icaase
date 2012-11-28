ad2577ac944b903eabd96905db1b208fc38459f2
Rody Middelkoop <rody.middelkoop@han.nl>
2012-11-28 23:16:28 +0100

    Spring WS Code Examples

--- spring-ws/HolidayService/README.md
+++
@@ -1,7 +0,0 @@
-Spring Webservices Example
-
-Features:
-* Uses spring-ws 2.0.2
-* Uses JDOM binding and XPATH parsing
-* Uses dependency injection of external service
-* Simple clean example using a one-way call (no response)
--- spring-ws/HolidayService/src/main/java/com/simple/spring/ws/SimpleHumanResourceService.java
+++
@@ -1,9 +0,0 @@
-package com.simple.spring.ws;
-
-import java.util.Date;
-
-public class SimpleHumanResourceService implements HumanResourceService {
-    public void bookHoliday(Date startDate, Date endDate, String name) {
-        System.out.println("Holiday booked for " + name);
-    }
-}
