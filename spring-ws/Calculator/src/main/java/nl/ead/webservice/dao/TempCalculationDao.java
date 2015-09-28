1331cf39eed5b8b15cc80a6113888fca7d7acc57
Rody Middelkoop <rody.middelkoop@gmail.com>
2015-09-28 10:32:27 +0200

    Missing temp dao pushed

---
+++ spring-ws/Calculator/src/main/java/nl/ead/webservice/dao/TempCalculationDao.java
@@ -0,0 +1,13 @@
+package nl.ead.webservice.dao;
+
+import nl.ead.webservice.model.Calculation;
+
+/**
+ * @author mdkr
+ * @version Copyright (c) 2014 HAN University, All rights reserved.
+ */
+public class TempCalculationDao implements ICalculationDao {
+    public void save(Calculation calculation) {
+        System.out.println("Doe niets");
+    }
+}
