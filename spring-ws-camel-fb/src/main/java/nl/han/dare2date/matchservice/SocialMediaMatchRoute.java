b44f5caf3d4bc9fb495dc49a618d641c959af506
Rody Middelkoop <rody.middelkoop@gmail.com>
2015-10-15 21:56:45 +0200

    Removed personal auth-keys, secrets and ids.

--- spring-ws-camel-fb/src/main/java/nl/han/dare2date/matchservice/SocialMediaMatchRoute.java
+++ spring-ws-camel-fb/src/main/java/nl/han/dare2date/matchservice/SocialMediaMatchRoute.java
@@ -15,19 +15,19 @@ public class SocialMediaMatchRoute extends RouteBuilder {
 
         from("spring-ws:rootqname:{http://www.han.nl/schemas/messages}MatchRequest?endpointMapping=#matchEndpointMapping")
         .setExchangePattern(ExchangePattern.InOut)
-        .split(ns.xpath("//mes:user/*"), new SocialMediaMatchAggregrate())
-            .parallelProcessing()
-            .convertBodyTo(String.class)
+        .split(ns.xpath("//mes:user/*"), new SocialMediaMatchAggregrate()) // split the request into four separate parts
+            .parallelProcessing() // fork
+            .convertBodyTo(String.class) // we'll handle with the request as XML using XPath
             .choice()
-                .when(body().contains("twitterName"))
+                .when(body().contains("twitterName")) // send the twitterName to twitter and wait for the aggregrate to do something useful
                     .log("Twitter: ${body}")
-                    .setHeader(TwitterConstants.TWITTER_KEYWORDS, ns.xpath("/mes:twitterName/text()", String.class))
+                    .setHeader(TwitterConstants.TWITTER_KEYWORDS, ns.xpath("/mes:twitterName/text()", String.class)) // fill the keyword parameter
                 .to("twitter://search")
-                .otherwise()
-                .setHeader("CamelFacebook.userId", ns.xpath("/mes:facebookid/text()", String.class))
+                .otherwise() // send the facebookid to FB and wait for the aggregrate to do something useful
+                .setHeader("CamelFacebook.userId", ns.xpath("/mes:facebookid/text()", String.class)) // fill the userid parameter
                 .to("facebook://user")
-            .end()
-        .end()
-        .marshal(jaxbMatchResponse);
+            .end() // end the parallel processing, this is a kind of "join"
+        .end() // stop splitting and start returning
+        .marshal(jaxbMatchResponse); // serialize the java-object from the aggregrator to SOAP/XML
     }
 }
